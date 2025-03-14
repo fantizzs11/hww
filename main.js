562cfea770fe3ef0b2dbe9fce618e37d0a0fa52b
fantizzs11 <ssanzarhonov@gmail.com>
2025-03-15 00:16:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function correctDate(date) {
+  return date < 10 ? "0" + date : date;
+}
+
+function saleDate(containerId) {
+    const container = document.getElementById(containerId);
+    const times = container.getElementsByClassName("time");
+  
+    const finishDate = new Date(2025, 3, 13 );
+    const currentDate = new Date();
+    const date = finishDate - currentDate;
+  
+      times[0].innerHTML = correctDate(Math.trunc(date / (1000 * 60 * 60 * 24)));
+      times[1].innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      times[2].innerHTML = correctDate(Math.trunc((date % (1000 * 60 * 60)) / (1000 * 60)));
+      times[3].innerHTML = correctDate(Math.trunc((date % (1000 * 60)) / 1000));
+
+  }
+
+
+setInterval(function () {  /*вызов функции каждую секунду*/ 
+  saleDate("timerFirst");
+  saleDate("timerSecond");
+}, 1000);
+
+startTimes();
+
+
